073378df9a76b67321a3af128af5a8ca267e372d
Haoxiang310 <gengh@usc.edu>
2023-07-01 19:08:41 -0700

    update

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,24 +1,26 @@
-import axios from 'axios';
-import { useState } from 'react';
+import axios from "axios";
+import { useState } from "react";
 
-export default ({ url, method, body, onSuccess }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
-  const doRequest = async(props = {} ) => {
+  const doRequest = async (props = {}) => {
     try {
       setErrors(null);
-      const response = await axios[method](url,{ ...body, ...props });
-      if(onSuccess) {
-        onSuccess(response.data)
+      const response = await axios[method](url, { ...body, ...props });
+
+      if (onSuccess) {
+        onSuccess(response.data);
       }
+
       return response.data;
     } catch (err) {
-      setErrors(  
-        <div className='alert alert-danger'>
+      setErrors(
+        <div className="alert alert-danger">
           <h4>Ooops....</h4>
-          <ul className='my-0'>
-            {err.response.data.errors.map((err) =>  
-            (<li key={err.message}>{err.message}</li>
+          <ul className="my-0">
+            {err.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
             ))}
           </ul>
         </div>
@@ -28,3 +30,5 @@ export default ({ url, method, body, onSuccess }) => {
 
   return { doRequest, errors };
 };
+
+export default useRequest;
--- client/pages/auth/signout.js
+++ client/pages/auth/signout.js
@@ -1,17 +1,20 @@
-import Router from "next/router";
 import { useEffect } from "react";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
-
-export default () => {
+const Signout = () => {
   const { doRequest } = useRequest({
-    url: '/api/users/signout',
-    method: 'post',
+    url: "/api/users/signout",
+    method: "post",
     body: {},
-    onSuccess: () => Router.push('/')
-  })
+    onSuccess: () => Router.push("/"),
+  });
+
   useEffect(() => {
     doRequest();
-  },[]);
-  return <div>Signing you out...</div>
-}
+  }, []);
+
+  return <div>Signing you out...</div>;
+};
+
+export default Signout;
